075d5072523181431d36079eecf78a25220d40ed
jun <jun@home>
2022-11-29 22:18:27 -0800

    truly i hate myself

--- test/2021-03.js
+++ test/2021-03.js
@@ -6,7 +6,7 @@ function getBitRate(nums) {
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
     for (let j = 0; j < num.length; j++) {
-      const curr = num[j];
+      const curr = +num[j];
       if (curr > 0) {
         res[j] += 1;
       } else {
@@ -44,6 +44,61 @@ async function getRates() {
   return res;
 }
 
+function getBitRateAtPosition(nums, position) {
+  let res = 0;
+  nums.forEach(num => {
+    const curr = +num[position];
+    if (curr > 0) {
+      res += 1;
+    } else {
+      res -= 1;
+    }
+  });
+  return res;
+}
+// filter down most common bit criteria until you reach a single value (if equal choose 1)
+function getOxygenGenRating(nums) {
+  let res;  
+  let filteredNums = nums;
+
+  for (let i = 0; i < nums[0].split('').length; i++) {
+    const moreCommonBit = getBitRateAtPosition(filteredNums, i) >= 0 ? 1 : 0;
+    filteredNums = filteredNums.filter(num => +num[i] === moreCommonBit);
+    if (filteredNums.length === 1) {
+      res = filteredNums[0];
+      break;
+    }
+  }
+
+  return res;
+}
+
+// least common, if equal choose 0
+function getCarbonScrubberRating(nums) {
+  let res;  
+  let filteredNums = nums;
+
+  for (let i = 0; i < nums[0].split('').length; i++) {
+    const lessCommonBit = getBitRateAtPosition(filteredNums, i) >= 0 ? 0 : 1;
+    filteredNums = filteredNums.filter(num => +num[i] === lessCommonBit);
+    if (filteredNums.length === 1) {
+      res = filteredNums[0];
+      break;
+    }
+  }
+
+  return res;
+
+}
+
+async function getLifeSupportRating() {
+  const nums = await readFile('./input.txt', String);
+  const res = binaryToDecimal(getOxygenGenRating(nums)) * binaryToDecimal(getCarbonScrubberRating(nums));
+  console.log(res)
+  return res;
+}
+
 module.exports = {
   getRates,
+  getLifeSupportRating,
 }
