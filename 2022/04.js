d978343d1ce95ef6beac0fa83c6fc914b4dd0105
jun <jun@home>
2022-12-03 21:19:46 -0800

    day 4

--- 2022/04.js
+++ 2022/04.js
@@ -1,8 +1,54 @@
 const { readFile } = require('../utils.js')
 
-async function foo(file='../input.txt') {
-  const lines = await readFile(file);
-  console.log(lines);
+function hasOverlap(line='') {
+  const [first, second] = line.split(',')
+  const firstRange = first.split('-').map(n => +n)
+  const secondRange = second.split('-').map(n => +n)
+  if (firstRange[0] >= secondRange[0] && firstRange[1] <= secondRange[1]) {
+    return true;
+  }
+  if (secondRange[0] >= firstRange[0] && secondRange[1] <= firstRange[1]) {
+    return true;
+  }
+  return false;
 }
 
-module.exports = foo;
+async function findFullyContains(file='../input.txt') {
+  const lines = await readFile(file);
+  let overlaps = 0;
+  lines.forEach((line) => {
+    if (hasOverlap(line)) {
+      overlaps += 1
+    }
+  })
+  console.log(overlaps)
+  return overlaps
+}
+
+function hasAnyOverlap(line='') {
+  const [first, second] = line.split(',')
+  const firstRange = first.split('-').map(n => +n)
+  const secondRange = second.split('-').map(n => +n)
+  if (firstRange[0] < secondRange[0] && firstRange[1] < secondRange[0]) {
+    return false;
+  }
+  if (secondRange[0] < firstRange[0] && secondRange[1] < firstRange[0]) {
+    return false;
+  }
+
+  return true;
+}
+
+async function findContains(file='../input.txt') {
+  const lines = await readFile(file);
+  let overlaps = 0;
+  lines.forEach((line) => {
+    if (hasAnyOverlap(line)) {
+      overlaps += 1
+    }
+  })
+  console.log(overlaps)
+  return overlaps
+}
+
+module.exports = findContains;
