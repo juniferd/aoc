f748307ec9a25a6487001c4a20625b6a941a55a3
jun <jun@home>
2022-12-16 07:15:59 -0800

    day 16 - attempt to make faster with bit operations

--- 2022/16.js
+++ 2022/16.js
@@ -19,36 +19,69 @@ function makeValves(lines) {
 }
 
 function runBFS(start, curr, dist, visited) {
-  queue = [[start, 0]];
+  queue = [[start, 0]]
 
-  let i = 0;
+  let i = 0
   while (i < queue.length) {
-    let [curr, dist] = queue[i++];
-    if (visited[curr] != undefined) { continue; }
-    visited[curr] = dist;
-    DISTANCES[start][curr] = dist++;
+    let [curr, dist] = queue[i++]
+    if (visited[curr] != undefined) {
+      continue
+    }
+    visited[curr] = dist
+    DISTANCES[start][curr] = dist++
     VALVES[curr].tunnels.forEach((neighbor) => {
-      queue.push([neighbor, dist]);
+      queue.push([neighbor, dist])
     })
-
-
   }
 }
 
 function findShortestPaths() {
   Object.keys(VALVES).forEach((curr) => {
-    runBFS(curr, curr, 0, {});
+    runBFS(curr, curr, 0, {})
   })
 }
 
 function trimGraph() {
   Object.keys(VALVES).forEach((curr) => {
     if (curr != 'AA' && VALVES[curr].rate == 0) {
-      delete VALVES[curr];
+      delete VALVES[curr]
     }
   })
 }
 
+function getMostPressure2(curr, timeLeft, pathsLeft, opened, cache) {
+  // return when timeLeft zero
+  // either open valve or go to neighbors
+  // go through all the tunnels
+  if (timeLeft <= 0 && pathsLeft === 0) return 0
+  if (timeLeft <= 0 && pathsLeft > 0) return getMostPressure2('AA', 26, 0, opened, cache)
+
+  const cacheKey = `${curr}-${timeLeft}-${pathsLeft}-${Array(...opened.keys()).sort()}`
+  if (cacheKey in cache) return cache[cacheKey]
+
+  const neighbors = VALVES[curr].tunnels
+  const rate = VALVES[curr].rate
+
+  const left = Math.max(
+    ...neighbors.map((neighbor) => {
+      return getMostPressure2(neighbor, timeLeft - 1,pathsLeft, opened, cache)
+      // opening valve
+    })
+  )
+  let right = 0
+  if (!opened.has(curr) && rate > 0) {
+    opened.add(curr)
+    right =
+      getMostPressure2(curr, timeLeft - 1, pathsLeft, opened, cache) +
+      (timeLeft - 1) * rate
+    opened.delete(curr)
+  }
+  res = Math.max(left, right)
+
+  cache[cacheKey] = res
+
+  return res
+}
 function getMostPressure(curr, timeLeft, opened, cache) {
   // return when timeLeft zero
   // either open valve or go to neighbors
@@ -63,21 +96,19 @@ function getMostPressure(curr, timeLeft, opened, cache) {
 
   const res = Math.max(
     ...neighbors.map((neighbor) => {
-      let left = 0
-      // not opening current valve
-      left = getMostPressure(neighbor, timeLeft - 1, opened, cache)
+      return getMostPressure(neighbor, timeLeft - 1, opened, cache)
       // opening valve
-      let right = 0
-      if (!opened.has(curr) && rate > 0) {
-        opened.add(curr)
-        right =
-          getMostPressure(neighbor, timeLeft - 2, opened, cache) +
-          (timeLeft - 1) * rate
-        opened.delete(curr)
-      }
-      return Math.max(left, right)
     })
   )
+  let right = 0
+  if (!opened.has(curr) && rate > 0) {
+    opened.add(curr)
+    right =
+      getMostPressure(neighbor, timeLeft - 2, opened, cache) +
+      (timeLeft - 1) * rate
+    opened.delete(curr)
+  }
+  return Math.max(left, right)
 
   cache[cacheKey] = res
 
@@ -98,23 +129,36 @@ function getMostPressureElephant(
   if (timeLeft <= 0) return 0
 
   if (eTimeLeft > timeLeft) {
-    return getMostPressureElephant(elephant, curr, eTimeLeft, timeLeft, opened, cache)
+    return getMostPressureElephant(
+      elephant,
+      curr,
+      eTimeLeft,
+      timeLeft,
+      opened,
+      cache
+    )
   }
 
-  const seen = Array( ...opened.keys()).sort()
+  const seen = Array(...opened.keys()).sort()
   const cacheKey = `${curr}-${elephant}-${timeLeft}-${eTimeLeft}-${seen}`
   if (cacheKey in cache) return cache[cacheKey]
   const otherCacheKey = `${elephant}-${curr}-${eTimeLeft}-${timeLeft}-${seen}`
-  if (otherCacheKey in cache) { return cache[otherCacheKey] }
+  if (otherCacheKey in cache) {
+    return cache[otherCacheKey]
+  }
 
   const neighbors = VALVES[curr].tunnels
   const rate = VALVES[curr].rate
 
   const left = Math.max(
     ...Object.keys(VALVES).map((neighbor) => {
-      if (neighbor == elephant || neighbor == curr || neighbor == 'AA') { return 0; }
-      if (opened.has(neighbor)) { return 0; }
-      let distance = DISTANCES[curr][neighbor];
+      if (neighbor == elephant || neighbor == curr || neighbor == 'AA') {
+        return 0
+      }
+      if (opened.has(neighbor)) {
+        return 0
+      }
+      let distance = DISTANCES[curr][neighbor]
       return getMostPressureElephant(
         neighbor,
         elephant,
@@ -130,7 +174,14 @@ function getMostPressureElephant(
   if (!opened.has(curr) && rate > 0) {
     opened.add(curr)
     right =
-      getMostPressureElephant(curr, elephant, timeLeft - 1, eTimeLeft, opened, cache) +
+      getMostPressureElephant(
+        curr,
+        elephant,
+        timeLeft - 1,
+        eTimeLeft,
+        opened,
+        cache
+      ) +
       (timeLeft - 1) * rate
     opened.delete(curr)
   }
@@ -141,20 +192,101 @@ function getMostPressureElephant(
   return res
 }
 
-function getMostPressureE(curr, timeLeft, pathsLeft, opened, cache) {
-  if (timeLeft <= 0 && pathsLeft === 0) return 0
-  if (timeLeft <= 0 && pathsLeft > 0) return getMostPressureE('AA', 26, pathsLeft - 1, opened, cache)
+const NODE_IDS = {}
 
-  const cacheKey = `${curr}-${timeLeft}-${pathsLeft}-${Array(...opened.keys()).sort()}`
+function markOpened(node, opened) {
+  const id = getNodeID(node)
+  const bit = 1 << id;  
+  return opened | bit;
+}
+
+function getNodeID(nodeID) {
+  if (!(nodeID in NODE_IDS)) {
+    NODE_IDS[nodeID] = Object.keys(NODE_IDS).length
+  }
+
+  return NODE_IDS[nodeID]  
+}
+
+function isOpened(node='', opened=1) {
+  const id = getNodeID(node);
+  const bit = 1 << id;
+  return opened & bit;
+}
+
+function unmarkOpened(node, opened) {
+  const id = getNodeID(node);
+  const bit = 1 << id;
+  return (~bit) & opened;
+}
+
+function getMostPressureEB(curr, timeLeft, pathsLeft, opened, cache) {
+  if (timeLeft <= 0 && pathsLeft === 0) return 0
+  if (timeLeft <= 0 && pathsLeft > 0)
+    return getMostPressureEB('AA', 26, pathsLeft - 1, opened, cache)
+
+  const cacheKey = `${curr}-${timeLeft}-${pathsLeft}-${opened}`
+
   if (cacheKey in cache) return cache[cacheKey]
 
   const rate = VALVES[curr].rate
 
   const left = Math.max(
     ...Object.keys(VALVES).map((neighbor) => {
-      if (neighbor == curr || neighbor == 'AA') { return 0; }
-      if (opened.has(neighbor)) { return 0; }
-      let distance = DISTANCES[curr][neighbor];
+      if (neighbor == curr || neighbor == 'AA') {
+        return 0
+      }
+      if (isOpened(neighbor, opened)) {
+        return 0
+      }
+      let distance = DISTANCES[curr][neighbor]
+      return getMostPressureEB(
+        neighbor,
+        timeLeft - distance,
+        pathsLeft,
+        opened,
+        cache
+      )
+    })
+  )
+
+  let right = 0
+  if (!isOpened(curr, opened) && rate > 0) {
+    opened = markOpened(curr, opened)
+    right =
+      getMostPressureEB(curr, timeLeft - 1, pathsLeft, opened, cache) +
+      (timeLeft - 1) * rate
+    opened = unmarkOpened(curr, opened)
+  }
+
+  const res = Math.max(left, right)
+
+  cache[cacheKey] = res
+
+  return res
+}
+function getMostPressureE(curr, timeLeft, pathsLeft, opened, cache) {
+  if (timeLeft <= 0 && pathsLeft === 0) return 0
+  if (timeLeft <= 0 && pathsLeft > 0)
+    return getMostPressureE('AA', 26, pathsLeft - 1, opened, cache)
+
+  const cacheKey = `${curr}-${timeLeft}-${pathsLeft}-${Array(
+    ...opened.keys()
+  ).sort()}`
+
+  if (cacheKey in cache) return cache[cacheKey]
+
+  const rate = VALVES[curr].rate
+
+  const left = Math.max(
+    ...Object.keys(VALVES).map((neighbor) => {
+      if (neighbor == curr || neighbor == 'AA') {
+        return 0
+      }
+      if (opened.has(neighbor)) {
+        return 0
+      }
+      let distance = DISTANCES[curr][neighbor]
       return getMostPressureE(
         neighbor,
         timeLeft - distance,
@@ -185,22 +317,21 @@ async function getMaxRelease(file = '../input.txt') {
   const lines = await readFile(file)
   makeValves(lines)
   findShortestPaths()
-  trimGraph();
+  trimGraph()
   console.log(VALVES)
 
-  console.log(Object.keys(VALVES));
+  console.log(Object.keys(VALVES))
   const opened = new Set()
   const cache = {}
   const cache2 = {}
   const start = +new Date()
   // const res = getMostPressureElephant('AA', 'AA', 26, 26, opened, cache)
-  const res = getMostPressureE('AA', 26, 1, opened, cache)
+
+  const res = getMostPressureEB('AA', 26, 1, 0, cache)
 
   const end = +new Date()
   console.log('TOOK', end - start)
   console.log(res)
-  
-  
 }
 
 module.exports = getMaxRelease
