2469d2bc97172d6a9b8f90f82ed03133b04ef2d8
jun <jun@home>
2024-11-30 21:26:39 -0800

    day 1 wooooo

--- 2024/01.js
+++ 2024/01.js
@@ -3,6 +3,62 @@ const { readFile } = require('../utils.js')
 async function getAnswer(file = '../input.txt') {
   const lines = await readFile(file)
   console.log('lines', lines)
+  const [firsts, seconds] = parse(lines)
+  // const tot = getDiffs(firsts, seconds)
+  // console.log(tot)
+  const tot = getSimilarity(firsts, seconds)
+  console.log(tot)
+}
+
+function getSimilarity(firsts, seconds) {
+  let total = 0
+  firsts.forEach((firstNum) => {
+    console.log('---')
+    console.log('firstnum', firstNum)
+    // go down seconds until you find match
+    const index = seconds.findIndex((second) => second === firstNum)
+    let count = 0
+    if (index >= 0) {
+      for (let i = index; i < seconds.length; i++) {
+        const secondNum = seconds[i]
+        console.log(secondNum)
+        if (secondNum === firstNum) {
+          count += 1
+        } else {
+          break
+        }
+      }
+    }
+    total += firstNum * count
+    console.log('total', total)
+  })
+  return total
+}
+
+function getDiffs(firsts, seconds) {
+  let tot = 0
+  firsts.forEach((firstNum, i) => {
+    const secondNum = seconds[i]
+    tot += Math.abs(firstNum - secondNum)
+  })
+  return tot
+}
+function parse(lines) {
+  let firsts = []
+  let seconds = []
+  lines.forEach((line) => {
+    let [first, second] = line.split('  ')
+    first = Number(first)
+    second = Number(second)
+    console.log(first, second)
+    firsts.push(first)
+    seconds.push(second)
+  })
+
+  // firsts.sort();
+  seconds.sort()
+
+  return [firsts, seconds]
 }
 
 // 2020 day 2 - warmup
@@ -25,12 +81,22 @@ function solve2020Day2Part2(lines) {
 function validatePosition(first, second, policy, password) {
   // exactly one position must contain policy
   // starts with index 1
-  let valid = (
+  let valid =
     (password[first - 1] === policy && password[second - 1] !== policy) ||
     (password[first - 1] !== policy && password[second - 1] === policy)
+  console.log(
+    'first',
+    first,
+    'second',
+    second,
+    'policy',
+    policy,
+    'password',
+    password,
+    'valid',
+    valid
   )
-  console.log('first', first, 'second', second, 'policy', policy, 'password', password, 'valid', valid)
-  
+
   return valid
 }
 
